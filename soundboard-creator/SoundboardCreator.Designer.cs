9587291c234bef73e6a1a27ad45dce2505669449
Patrick McSherry <mcsherry.patrick@gmail.com>
2020-05-17 16:51:39 -0700

    added initial files to upload
    initial upload

---
+++ soundboard-creator/SoundboardCreator.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace soundboard_creator
+{
+    partial class SoundboardCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelFileSelect = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(37, 124);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 1;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(133, 124);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(75, 23);
+            this.createBtn.TabIndex = 2;
+            this.createBtn.Text = "Create";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please browse for a file directory with audio files";
+            // 
+            // labelFileSelect
+            // 
+            this.labelFileSelect.AutoSize = true;
+            this.labelFileSelect.Location = new System.Drawing.Point(12, 87);
+            this.labelFileSelect.Name = "labelFileSelect";
+            this.labelFileSelect.Size = new System.Drawing.Size(0, 13);
+            this.labelFileSelect.TabIndex = 4;
+            // 
+            // SoundboardCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 159);
+            this.Controls.Add(this.labelFileSelect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.browseBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SoundboardCreator";
+            this.Text = "Soundboard Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelFileSelect;
+    }
+}
+
---
+++ soundboard-creator/SoundboardCreator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace soundboard_creator
+{
+    public partial class SoundboardCreator : Form
+    {
+        static String directory = "";
+        public SoundboardCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+            if (openFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelFileSelect.Text = "Folder Selected";
+                directory = openFolderDialog.SelectedPath;
+            }
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            if (!directory.Equals(""))
+            {
+                string[] files = System.IO.Directory.GetFiles(directory);
+                string soundboardhtml = "<!doctype html>";
+                soundboardhtml += "<html><head><title>Soundboard</title></head>\n";
+                soundboardhtml += " <body>\n";
+                soundboardhtml += "<script>\n";
+                soundboardhtml += "var music = new Audio(); \n";
+                soundboardhtml += "function playMusic(file) { music.pause(); music = new Audio(file); music.play(); }\n";
+                soundboardhtml += "function play(id) {playMusic(document.getElementById(id).src); }\n";
+                soundboardhtml += "</script>";
+                int count = 0;
+                foreach (string file in files)
+                {
+                    if(file.Contains(".mp3") || file.Contains(".ogg") || file.Contains(".wav"))
+                    {
+                        count++;
+                        string id = "audio" + count;
+                        string filename = Path.GetFileNameWithoutExtension(file);
+                        filename = filename.Replace(@"\\" , @"\");
+                        soundboardhtml += "<input type =" + '"' + "button" + '"' + " value=" + '"' + filename + '"' + "onclick=" + '"' + "play('" + id + "')" + '"' + ">\n";
+                        soundboardhtml += "<audio id=" + '"' + id + '"' + " src="+ '"' + file + '"' + "></audio>\n";
+                    }
+                }
+                soundboardhtml += "</body></html>";
+                using (StreamWriter writer = new StreamWriter(directory + "\\soundboard.html"))
+                {
+                    writer.Write(soundboardhtml);
+                }
+                MessageBox.Show("Complete");
+            }
+        }
+    }
+}
